6fbc158ce02368d11fdec7280ed13162a59d95b3
lualeet <43719546+lualeet@users.noreply.github.com>
2023-08-09 12:18:05 +0300

    add README

---
+++ README.md
@@ -0,0 +1,31 @@
+# sptarkov-deadzone
+An SPT mod which adds insurgency style deadzone/free aim.
+
+# Building
+- ## Copy DLLs
+    ### Automated method (currently linux only)
+    - symlink ./dependencies/source to point to your spt path.
+        ```bash
+        ln -s ${PATH_TO_SPT} ./dependencies/source
+        ```
+    run `update_bin.sh`.
+    ### Manual method
+    Find all required dlls in your SPT folder, and copy them to the ``dependencies`` folder.
+
+    For a list of all required dlls, open ``update_bin.sh`` in a text editor and find
+    ```bash
+    # List of required assemblies
+    populateFile "0Harmony";
+    populateFile "Aki.Common";
+               ...
+               ...
+               ...
+    ```
+
+- ## Compile
+    run `dotnet build`.
+
+    output assembly wil be output to ``./bin/Debug/net472/DeadzoneMod.dll``.
+
+# Installing
+Copy ``DeadzoneMod.dll`` to ``$SPT/BepInEx/plugins/``.
--- update_bin.sh
+++ update_bin.sh
@@ -1,10 +1,38 @@
 #!/usr/bin/env bash
-
-# ./dependencies/source expected to be symlink to main spt path
-
 set -e
 
+if ! [[ -d "./dependencies/source" ]]; then
+    echo "Expected ./dependencies/source to be a directory!"
+    echo "source should be a symlink to your spt directory."
+    exit 1;
+fi
+
+
 cd ./dependencies
+
+if [[ "$(ls | wc -l)" == "1" ]]; then
+    # pre-populate dummy files
+    function populateFile () {
+        touch "$1.dll";
+    }
+
+    # List of required assemblies
+    # ".dll" is automatically postfixed
+    populateFile "0Harmony";
+    populateFile "Aki.Common";
+    populateFile "Aki.Reflection";
+    populateFile "Assembly-CSharp";
+    populateFile "BepInEx";
+    populateFile "Comfort";
+    populateFile "Newtonsoft.Json";
+    populateFile "UnityEngine";
+    populateFile "UnityEngine.AudioModule";
+    populateFile "UnityEngine.CoreModule";
+    populateFile "UnityEngine.InputLegacyModule";
+    populateFile "UnityEngine.InputModule";
+fi
+
+
 
 function copyFile () {
     cp -v "$2" "$1"
@@ -13,7 +41,7 @@ function copyFile () {
 export -f copyFile
 
 for file in *; do
-    if [ -f "$file" ]; then
+    if [[ -f "$file" ]]; then
         # most readable bash code
         find -L ./source -name "$file" -exec bash -c "copyFile \"$file\" \"\$@\"" bash {} +
     fi
